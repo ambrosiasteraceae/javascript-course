6f5569bf28fa08d02d3ea879403482f396523146
ambrosiasteraceae <66112305+ambrosiasteraceae@users.noreply.github.com>
2025-05-17 15:08:09 +0400

    Update Timer Class for timeElement display

--- pomos/src/timer.js
+++ pomos/src/timer.js
@@ -1,11 +1,17 @@
 import {getUnixTime, format} from "date-fns";
-import  {adjustTime} from "./index.js"
+import  {adjustTime, refreshTime} from "./index.js";
+
+export const  HALFHOUR = 1800000;
+export const  FIFTEEN = 15000;
+export const  FIVE = 5000;
+
 // const fns = require("date-fns");
+
 /*
-Rule of Thumb
-Whenever you pass a class method as a callback, 
-wrap it or bind it, because JavaScript
-doesn't automatically carry this along for the ride.
+    Rule of Thumb
+    Whenever you pass a class method as a callback, 
+    wrap it or bind it, because JavaScript
+    doesn't automatically carry this along for the ride.
 */
 
 export default class Timer{
@@ -13,8 +19,9 @@ export default class Timer{
     //A timer is assigned to each task, but this does not mean the timer starts when it is instantiated
     constructor(timeInMiliseconds){
         // this.isPaused = false;
-        // this.original = timeInMiliseconds;
-        // this.duration = timeInMiliseconds;
+        this.original = timeInMiliseconds;
+        this.duration = timeInMiliseconds;
+        
         // this.task = task;
         // this.intervalId;
         //I need to dig about this issue deeper to understand why it works
@@ -22,16 +29,23 @@ export default class Timer{
     }
 
     refresh(){ 
-        console.log("Refresh was callled:", this.getTime())
         this.duration = this.original;
-        adjustTime();
+        refreshTime();
+        console.log("Refresh was callled:", this.getTime())
+        // this.pause()
+        // adjustTime();
     }
 
     start(){
-        
+        if (this.duration == 0)
+            return
         console.log("Started at:", this.getTime())
-        if(!this.intervalId)       
+        if(!this.intervalId)
+        {
             this.intervalId = setInterval(this.parseTime, 1000);
+            return
+        }      
+
     }
     
     decrement(){
@@ -46,9 +60,11 @@ export default class Timer{
     this.decrement();
     adjustTime();
     console.log(this.getTime());
-
     if (this.duration == 0)
+    {
         this.pause()
+        setTimeout(() => this.refresh(), 1000);
+    }
     }
         
     pause(){
