67922ce191cef7086d4fb30c1001ccdb97aa12c0
seculinus <66112305+seculinus@users.noreply.github.com>
2025-05-20 00:52:23 +0400

    Test DisplayManager & DOM Task Creation
    - Fix activeProjectID when adding project;
    - DisplayManager surprisingly switches and reupdates the DOM on project selection;
    - DisplayManager addTaskElement works and  appends a new task at the end based on current project;
    - I think the controll for this should be given to the app controller so that both the project and the display are change simulatenously;

--- pomos/src/index.js
+++ pomos/src/index.js
@@ -1,32 +1,10 @@
-/*Question
-
-1. How do we fetch the current task and change time accordingly?;
-     - a variable that gets changed when a task is switched?
-     - project getActiveTask();
-2. How do we switch task and make sure the time is diplayed for the switched task?
-3. How do we increment the pomodoros when a task is finished?
-4. The event which handles the 
-5. Each Task having its own timer seems like a unoptimized  memory wise:
-    - when an active task is changed, you should be able to store the timeDuration,
-    - and load the timer from that point onwards
-6. The million dollar question is how can we register an event to dispatch from inside 
-    - the Timer class while keeping separaion of concerns. Decorators? delegators?
-*/
-
-/* 
-Issues
-- a task that is finished can be started with countdown;
-- multiple timers can be be played simultaneously
-- task list needs to be updated when a task is completed or when a task is advancing, on the dom side
-*/
-
 
 import "./styles.css";
 import Task from "./task.js";
 import Project from "./project.js";
 import Timer, {HALFHOUR, FIFTEEN,FIVE} from "./timer.js"
 import "./tests.js";
-import DisplayManager, {BaseElement,  TaskElement} from "./display.js";
+import DisplayManager, {BaseElement,  ProjectManager,  TaskElement} from "./display.js";
 
 
 
@@ -102,47 +80,57 @@ logBtn.addEventListener("click",() => activeTask?.print())
 // const timer = new  Timer( FIVE);
 // const task = new Task(options, timer);
 
-// //Init  Display
-// // updateTime();
 
-const project1 = new Project("trial");
-activeProject = project1;
-
-project1.generateExamples(10);
+const foo = new Project("foo");
+const bar = new Project("bar");
 
 
-// project1.tasks[3].isWorkedOn = true;
-// switchActiveTask()
-// console.log(activeTask)
-// project1.tasks[4].isWorkedOn = true;
-// switchActiveTask()
-// console.log(activeTask)
+foo.generateExamples(7);
+bar.generateExamples(4);
 
-// ****** VERSION 1 ********
-
-// const manager = new DisplayManager();
-// manager.addProject(project1);
-// manager.populateProject();
 
 
 // ****** VERSION 2 ********
-const manager = new DisplayManager();
-
-manager.display(project1);
 
 
+const dm = new DisplayManager();
+const pm = new ProjectManager();
 
-// const projElement = new BaseElement(".projects");
+pm.addProject(foo);
+console.log("Current active project id:");
+console.log(pm.activeKey);
+pm.addProject(bar);
+console.log("Current active project id:");
+console.log(pm.activeKey);
+console.log("Current project");
+console.log(pm.getActiveProject()); 
 
-// console.log("Project is: ");
-// console.log(projElement);
 
-// console.log(document.querySelector(".projects"));
-// activeTask = project1.tasks[3];
-// console.log("activbe[ask before task element", activeTask)
-// const ate = new TaskElement(activeTask);
-// console.log("Ate is");
-// console.log(ate);
+
+
+// console.log("New project after switch");
+// console.log(pm.getActiveProject()); 
+
+// dm.display(pm.getActiveProject());
+
+
+pm.switchProject(1);
+const options = {name: "Pomo App", pomodoros:12};
+const timer = new  Timer( FIVE);
+const task = new Task(options, timer);
+
+
+dm.display(pm.getActiveProject())
+
+setTimeout(() =>{
+    pm.getActiveProject().addTask(task);
+    dm.createTaskElement(task);
+    // pm.switchProject(1);
+    dm.display(pm.getActiveProject())
+}, 2000);
+
+// dm.display(pm.getActiveProject());
+
 
 // const activeTaskElement = document.querySelector("input[name=task]:checked");
 const activeTaskElement = document.querySelectorAll(".radio");
@@ -150,7 +138,7 @@ const activeTaskElement = document.querySelectorAll(".radio");
 activeTaskElement.forEach((radioElem) => {radioElem.addEventListener("change", (e)=>{
     const taskDiv = e.target.parentNode;
     const taskID = taskDiv.dataset.key;
-    const newTask = project1.getTaskbyID(taskID)
+    const newTask = pm.getActiveProject().getTaskbyID(taskID)
     switchActiveTask(newTask);
 
 })});
