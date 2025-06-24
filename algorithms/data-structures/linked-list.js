12d2705c14282dc46c631a52c18ecef167574a06
seculinus <66112305+seculinus@users.noreply.github.com>
2025-06-24 20:58:48 +0400

    feat: implement find contains remove insert for ll

--- algorithms/data-structures/linked-list.js
+++ algorithms/data-structures/linked-list.js
@@ -1,5 +1,3 @@
-// import { mergeSort } from "../recursive";
-
 
 class Node{
     constructor(){
@@ -11,23 +9,8 @@ class Node{
 class LinkedList{
     constructor(list){
         this.build(list);
+        this.size = this.getSize();
     
-    }
-
-    traverse(node = this.node){
-        if(node.nextNode != null)
-            return this.traverse(node.nextNode)
-        return node
-
-    }
-
-    get head(){
-        return this.node
-    }
-
-    get tail(){
-        return this.traverse()
-
     }
 
     build(list){
@@ -49,8 +32,7 @@ class LinkedList{
 
         const newNode = new Node();
         newNode.value = value;
-        this.tail.nextNode = newNode;
-        
+        this.tail.nextNode = newNode;        
     }
 
     prepend(value){
@@ -60,27 +42,136 @@ class LinkedList{
         this.node = newNode;
     }
 
-//uses this attributes
-    // toString(node = this.node){
-    //     if(node.nextNode != null){
-    //         this.string += `(${node.value}) -> `
-    //         this.toString(node.nextNode)}
-    //     else       
-    //         this.string +=`(${node.value}) -> null`
-    // }
+    getSize(node = this.node, count = 1){
+        if(node.nextNode != null)
+            return this.getSize(node.nextNode, ++count)
+        return count
+    }
+
+    get head(){
+        return this.node
+    }
+
+    get tail(){
+        return this.traverse()
+    }
+
+    pop(node = this.node){
+        if(node.nextNode.nextNode == null)
+        {
+            const poppedNode = node.nextNode;
+            node.nextNode = null;
+            return poppedNode;
+        }
+        return this.pop(node.nextNode)
+        
+    }
+
+
+    contains(value, node = this.node){    
+        if(node.value == value)
+            return true
+        else if(node.nextNode != null)
+            return this.contains(value, node.nextNode) 
+        else
+            return false
+    }
+    
+    find(value, node = this.node,  index = 0){
+        if(node.value == value)
+            return index
+        else if(node.nextNode != null)
+            return this.find(value, node.nextNode, ++index)
+        else
+            return null
+    }
+
+
+    getNodeAtIndex(index, node = this.node, currIdx = 0){
+        if(index == currIdx)
+            return node
+        else if (node.nextNode!= null)
+            return this.getNodeAtIndex(index, node.nextNode, ++currIdx)
+        else
+        {
+            console.error("Index out of range")
+            return null
+        }
+    }
+
+    insertAt(value, index){
+        //cut the nodes
+        if(index == 0)
+            return this.prepend(value)
+
+        const leftSide = this.getNodeAtIndex(index-1);
+        const temp = leftSide.nextNode;
+        const newNode = new Node();
+        newNode.value = value;
+        leftSide.nextNode = newNode;
+        newNode.nextNode = temp;
+    }
+    
+    removeAt(index){
+        const leftSide = this.getNodeAtIndex(index-1)
+        const rightSide = this.getNodeAtIndex(index+1)
+        leftSide.nextNode = rightSide;
+
+
+
+    }
+
+    traverse(node = this.node){
+        if(node.nextNode != null)
+            return this.traverse(node.nextNode)
+        return node
+
+    }
 
     toString(node = this.node, string = ""){
-        let s = string + `(${node.value}) -> `; 
+        let s = string + `( ${node.value} ) -> `; 
         if(node.nextNode != null){ 
             return this.toString(node.nextNode, s)}
         else
             return s + "null"
-    
+
     }
 }
 
-const ll = new LinkedList([1,2,4,5])
-// console.log(ll.node)
+const ll = new LinkedList([15, 1,2 ,4, 7, 9])
+
+
+// CONTAINS TESTS
+// console.log(ll.contains(1))
+// console.log(ll.contains(4))
+// console.log(ll.contains(101))
+// console.log(ll.contains(9))
+//FIND TESTS
+// console.log("*******")
+// console.log(ll.find(5))
+// console.log(ll.find(2))
+// console.log(ll.find(9))
+// console.log(ll.find(5))
+// ll.pop()
+
+console.log(ll.toString())
+
+ll.insertAt(21, 5)
+console.log(ll.toString())
+
+
+ll.removeAt(2)
+console.log(ll.toString())
+
+ll.removeAt(4)
+console.log(ll.toString())
+
+
+
+// ll.pop()
+// ll.pop()
+// console.log(ll.toString())
+// // console.log(ll.node)
 // console.log(ll.node.nextNode);
 // console.log(ll.node.nextNode.nextNode);
 // console.log(ll.node.nextNode.nextNode.nextNode);
@@ -89,14 +180,20 @@ const ll = new LinkedList([1,2,4,5])
 // console.log(ll.toString())
 // console.log(ll.string)
 
-console.log(ll.toString())
-console.log("head is ", ll.head)
-console.log("tail is", ll.tail)
+// console.log(ll.toString())
+// console.log("head is ", ll.head)
+// console.log("tail is", ll.tail)
 
-ll.append(7);
-console.log(ll)
-console.log(ll.toString())
 
-ll.prepend(7);
-console.log(ll)
-console.log(ll.toString())
+// ll.append(81);
+// ll.prepend(15);
+
+
+// console.log("Before pop")
+// console.log(ll.toString())
+
+// console.log("popped item is " , ll.pop())
+// console.log("After pop")
+// console.log(ll.toString())
+
+
