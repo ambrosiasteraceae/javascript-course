487b464383c27354f52495b687f2888d3da28345
seculinus <66112305+seculinus@users.noreply.github.com>
2025-06-25 18:05:27 +0400

    feat:balanced search tree init
    build,find and insert work. what doesnt work is the get parent method

---
+++ algorithms/data-structures/balanced-search-tree.js
@@ -0,0 +1,173 @@
+import {Node, prettyPrint } from "./index.js";
+
+
+class BalancedSearchTree{
+    constructor(array){
+        this.sorted = this.initialize(array)
+        this.root = this.buildTree(this.sorted);
+    }
+    initialize(array){
+        const unique = [...new Set(array)]
+        const sorted = unique.sort((a,b)=> (a - b))
+
+        return sorted
+    }
+    buildTree(array, node = null,){
+        
+        let mid = Math.floor(array.length/2)
+        let value = array[mid]
+        if(value == undefined)
+            return node
+        node = new Node(value) 
+        if(array.length <= 1)
+            return node
+        node.left = this.buildTree(array.slice(0, mid), node.left)
+        node.right =  this.buildTree(array.slice(mid+1), node.right)
+        return node
+
+    }
+
+    insert(value, node = this.root){
+        if(node.value < value)
+        {
+            if(node.right)
+                this.insert(value, node.right)
+            else
+                node.right = new Node(value)
+        }
+        else
+        {
+            if(node.left)
+                this.insert(value, node.left)
+            else
+                node.left = new Node(value)
+        }
+    }
+
+    find(value, node = this.root){
+        //the problem with this is that it only checks left side only  
+
+        //returns the node 
+        if(!node)
+            return -1
+
+        if (node.value == value)
+            return node
+        //search left   
+        if(node.value < value)
+            return this.find(value, node.right)
+        //search right
+        else 
+            return this.find(value, node.left)
+        // if all fails and we parsed all tree we return -1
+    }
+
+    getParent(value, node = this.root){
+        if(!node)
+            return -1
+        // if(!node.left)
+        //     return this.getParent(value, node.right)
+        // if(!node.right)
+        //     return this.getParent(value, node.left)
+        console.log("Node left value with value", node.left.value, value)
+        if (value == node.left.value || value == node.right.value)
+            return node
+        if (node.left.value >= value)
+            return this.getParent(value, node.left)
+        else
+            return this.getParent(value, node.right)
+        
+        // return this.getParent(child, node.left/node.right)
+    }
+    
+    deleteItem(value){
+
+        /*
+
+        -> find node
+        -> check for left/right and return 1-3 case;
+        -> get maximum of leftSubTrree fn();
+        -> get minimum of rrightSubTree fn();
+        
+        */
+
+        let expr;
+        const node = this.find(value);
+
+        if(node == -1)
+            return 
+
+        if(node.left && node.right)
+            expr = 3
+        else if (node.left || node.right)
+            expr = 2
+        else
+            expr = 1
+        
+    
+        switch(expr){
+        case 1:
+            if(value < parent.value)
+                parent.left = null;
+            parent.right = null;
+            break;
+        }
+
+
+        //case 1
+        //a leaf node -> nothing happens
+
+
+        //case2
+        //a node with 1 child -> make the child point to the deleted node parent
+        
+        
+        //case3
+        //a node with more than 1 child
+        // -> so find the node that you want to delete.
+        // -> traverse this tree for the one which is immediately higher
+        // -> you do this by going firrst right and then traversing its left counterpart.
+        // -> you move that value to be the one inside that tree
+    }
+
+
+    levelOrder(){}
+    inOrder(){}
+    preOrder(){}
+    postOrder(){}
+    get height(){}
+    get depth(){}
+    isBalanced(){}
+    rebalance(){}
+}
+const input0 = [1,2,3,4,5,6,7]
+const input1 = [50, 30, 20, 40, 32, 34, 36, 70, 60 ,65, 80, 75, 85]
+const mm = new BalancedSearchTree(input1)
+
+
+/*
+│       ┌── 85
+│       │   └── 80
+│   ┌── 75
+│   │   │   ┌── 70
+│   │   └── 65
+│   │       └── 60
+└── 50
+    │   ┌── 40
+    │   │   └── 36
+    └── 34
+        │   ┌── 32
+        └── 30
+            └── 20
+*/
+
+// console.log(mm.find(20))
+// mm.deleteItem(20)
+// console.log(prettyPrint(mm.root))
+console.log("Parent is: ", mm.getParent(30).value)
+ 
+// mm.insert(41)
+// mm.insert(42)
+// mm.insert(19)
+// mm.insert(86)
+// console.log(prettyPrint(mm.root))
